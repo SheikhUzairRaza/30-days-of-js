6f25f6704e18d91d03530b3d78e663a3b4106c82
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2026-03-09 00:12:47 +0500

    fix remove_duplicates code

--- remove_duplicates.js
+++ remove_duplicates.js
@@ -1,9 +1,14 @@
-//remove duplicates from non-dec sorted array
-let arr = [0,0,1,2,3]
-for (let j = 1; j <= n; j++) {
-  let i = 0
-  if (arr[i] !== arr[j]) {
-    i++
-    arr[i] = arr[j]
+//remove duplicates from non-dec sorted array & return count of unique elements 
+function removeDuplicatesFromNonDecSortedArray(arr) {
+    let i = 0
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++
+      arr[i] = arr[j]
+      }
     }
+    return i+1;
 }
+
+let arr = [0, 0, 1, 2, 3]
+console.log(removeDuplicatesFromNonDecSortedArray(arr))
